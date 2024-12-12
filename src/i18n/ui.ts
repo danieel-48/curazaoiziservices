f11b4d3c09eae95cc7b66f0a98150c80aff2b999
jeracode <juanestebanramirezavila@gmail.com>
2024-12-12 14:03:26 -0500

    Entrega al cucho con ajustes css y cositas

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = { 
+    en: 'English',
+    nl: 'Nederlands',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    nl: {
+        'nav.index': 'THUIS',
+        'nav.about': 'OVER',
+        'nav.services': 'DIENSTEN',
+        'nav.prices': 'PRIJZEN',
+        'nav.faqs': 'FAQs',
+        'nav.contact': 'CONTACT',
+    },
+    en: {
+      'nav.index': 'HOME',
+      'nav.about': 'ABOUT',
+      'nav.services': 'SERVICES',
+      'nav.prices': 'PRICES',
+      'nav.faqs': 'FAQs',
+      'nav.contact': 'CONTACT',
+    },
+  } as const;
